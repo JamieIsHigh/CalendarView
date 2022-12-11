cca931dd9af004a2f68e5df74e82008418ae6bc4
Jamie <ishighjamie@gmail.com>
2022-12-12 00:55:03 +0100

    Buttons basic setup

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.widget.Button
 import android.widget.CalendarView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
@@ -13,11 +14,36 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val calendarView = findViewById<CalendarView>(R.id.calendarView)
+        val wyjazdButton = findViewById<Button>(R.id.wyjazdButton)
+        val powrotButton = findViewById<Button>(R.id.powrotButton)
+        val wyjazd = findViewById<TextView>(R.id.wyjazd)
+        val powrot = findViewById<TextView>(R.id.powrot)
+        val naglowek = findViewById<TextView>(R.id.naglowek)
+
+        var isWyjazdSet = false
+        var isPowrotSet = false
+        var isWyjazdSetting = false
+        var isPowrotSetting = false
+
+        var dataWyjazdu = 1000000000
+        var dataPowrotu = 1000000000
+
 
         val format = mutableListOf<SimpleDateFormat>(SimpleDateFormat("yyyy"), SimpleDateFormat("MM"), SimpleDateFormat("dd"))
         var today = mutableListOf<Int>(format[0].format(Date()).toInt(),format[1].format(Date()).toInt() -1,format[2].format(Date()).toInt());
 
         calendarView.setMinDate(Date().time)
         calendarView.setMaxDate(Date().time + 63115200000)
+
+        wyjazdButton.setOnClickListener {
+            if (!isWyjazdSetting) {
+                naglowek.text = "Wybierz na kalendarzu datę wyjazdu: "
+                wyjazdButton.text = "Anuluj"
+                isWyjazdSetting = true
+            } else {
+                wyjazdButton.text = "Ustaw/zmień datę wyjazdu"
+                isWyjazdSetting = false
+            }
+        }
     }
 }
